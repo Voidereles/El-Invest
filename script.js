a6440daedab93df2e115e8e6eb34d13ecc0460cf
Maciej <maciej.zelazo@protonmail.com>
2020-02-04 22:24:42 +0100

    components made

---
+++ script.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function () {
+    $('.select2-single').select2({
+        placeholder: "Placeholder"
+    });
+});
+
+$(document).ready(function () {
+    $('.select2-multiple').select2({
+        placeholder: "Placeholder"
+    });
+});
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(".range-single").ionRangeSlider({
+    skin: "round",
+    min: 0,
+    max: 1000,
+    from: 500
+});
+
+$(".range-double").ionRangeSlider({
+    type: "double",
+    skin: "round",
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 800
+});
